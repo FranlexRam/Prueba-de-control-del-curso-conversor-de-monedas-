1ae2db2d07991bdce4123edabdeb8e38f49800b5
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-01-02 21:56:06 -0400

    Forma manual de agregar datos.

---
+++ js/index.js
@@ -0,0 +1,22 @@
+/*
+Dólares USA - 1.06
+Libras esterlinas - 0.87
+Pesos argentinos - 370.22
+Pesos colombianos - 4510.51
+Pesos mexicanos - 19.33
+*/
+/*
+const CAMBIOS=[
+    {moneda:"Dólares USA",elCambio:1.06},
+    {moneda:"Libras esterlinas",elCambio:0.87},
+    {moneda:"Pesos argentinos",elCambio:370.22},
+    {moneda:"Pesos colombianos",elCambio:4510.51},
+    {moneda:"Pesos mexicanos",elCambio:19.33},
+]
+*/
+let select = document.querySelector("select");
+select.insertAdjacentHTML("beforeend",`<option value="1.06">Dólares USA</option>`)
+select.insertAdjacentHTML("beforeend",`<option value="0.87">Libras esterlinas</option>`)
+select.insertAdjacentHTML("beforeend",`<option value="370.22">Pesos argentinos</option>`)
+select.insertAdjacentHTML("beforeend",`<option value="4510.51">Pesos colombianos</option>`)
+select.insertAdjacentHTML("beforeend",`<option value="19.33">Pesos mexicanos</option>`)
