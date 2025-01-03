811446b86e32da8540995492db9a2e58388c8cfc
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-01-02 22:29:31 -0400

    Aprendiendo: .map y selectedIndex.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
-/*
-Dólares USA - 1.06
-Libras esterlinas - 0.87
-Pesos argentinos - 370.22
-Pesos colombianos - 4510.51
-Pesos mexicanos - 19.33
-*/
-/*
+let select = document.querySelector("select");
+let cambio;
+
 const CAMBIOS=[
     {moneda:"Dólares USA",elCambio:1.06},
     {moneda:"Libras esterlinas",elCambio:0.87},
@@ -13,10 +8,13 @@ const CAMBIOS=[
     {moneda:"Pesos colombianos",elCambio:4510.51},
     {moneda:"Pesos mexicanos",elCambio:19.33},
 ]
-*/
-let select = document.querySelector("select");
-select.insertAdjacentHTML("beforeend",`<option value="1.06">Dólares USA</option>`)
-select.insertAdjacentHTML("beforeend",`<option value="0.87">Libras esterlinas</option>`)
-select.insertAdjacentHTML("beforeend",`<option value="370.22">Pesos argentinos</option>`)
-select.insertAdjacentHTML("beforeend",`<option value="4510.51">Pesos colombianos</option>`)
-select.insertAdjacentHTML("beforeend",`<option value="19.33">Pesos mexicanos</option>`)
+CAMBIOS.map(valor => select.insertAdjacentHTML("beforeend",`<option value="${valor.elCambio}">${valor.moneda}</option>`));
+modificarCambio();
+select.addEventListener("change", modificarCambio)
+
+function modificarCambio() {
+    cambio = select.value;
+    const INDICE = select.selectedIndex;
+    const MONEDA = select[INDICE].text.toLowerCase();
+    document.querySelector("#cambio").innerHTML=`1€ es ${cambio} ${MONEDA}`;
+}
